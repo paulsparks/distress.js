b7c82bfe6c7f8fabfa10769e342329e45cda7906
paulsparks <psparks1225@gmail.com>
2025-07-26 10:42:43 -0400

    html stuff

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import { App } from "./app";
+import * as fs from "node:fs";
 
 const app = new App();
 
 app.get("/profile/:userId/post/:postId", (req, res) => {
+    res.writeHead(200, { "content-type": "text/html" });
     res.end(
         `<h1>User ID: ${req.params.userId}</h1><h1>Post ID: ${req.params.postId}</h1>`
     );
 });
 
 app.get("/", (req, res) => {
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(
-        JSON.stringify({
-            hello: "world",
-        })
-    );
+    fs.readFile("routes/index.html", (err, data) => {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(data);
+    });
 });
 
 app.listen(3000);
